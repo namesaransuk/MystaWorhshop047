5e31f8163e5075075492b4590d162043dbb9a7ae
namesaransuk <49622873+namesaransuk@users.noreply.github.com>
2020-11-10 12:34:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const PORT = process.env.PORT || 8080;
 const express = require('express');
 const indexRouter = require('./routes/index');
 const path = require('path');
-const morgan = require('morgan');
-const cors = require('cors');
-const dotenv = require('dotenv').config();
 
 // 1. Configuration here
+
+
+
 const fetch = require('node-fetch');
 global.fetch = fetch;
 
 const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT|| 8080;
 
 //Middleware เพื่ออ่าน req.body
 app.use(express.json());
